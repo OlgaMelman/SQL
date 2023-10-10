b15577263e6ede4e0e0f6a1de230f725b8ebf3a7
Olga Melman <olga.melman198237@gmail.com>
2023-10-10 15:11:04 +0300

    added tests

---
+++ src/test/java/ru/netology/loginbank/test/LoginBankTest.java
@@ -0,0 +1,56 @@
+package ru.netology.loginbank.test;
+
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.loginbank.data.DataHelper;
+import ru.netology.loginbank.data.SQLHelper;
+import ru.netology.loginbank.page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.loginbank.data.SQLHelper.cleanDatabase;
+
+public class LoginBankTest {
+
+    @AfterAll
+    static void teardown() {
+
+        cleanDatabase();
+    }
+
+    @Test
+    void shouldSuccessfulLogin() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVisiblity();
+        var verificationCode = SQLHelper.getVerificationCode();
+        verificationPage.validVerify(verificationCode.getCode());
+    }
+
+    @Test
+    void shouldErrorMassageWhenWrongCode() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVisiblity();
+        var verificationCode = DataHelper.generateRandomVerificationCode();
+        verificationPage.verify(verificationCode.getCode());
+        verificationPage.verifyErrorNotificationVisiblity();
+
+    }
+
+    @Test
+    void shouldErrorIfWrongUser() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = DataHelper.generateRandomUser();
+        loginPage.validLogin(authInfo);
+        loginPage.verifyErrorNotificationVisibility();
+
+    }
+
+}
+
